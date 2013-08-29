0324d2fd00c52f394b8e799ad2edd1e839f6b53e
Francisco Mayoral <fmayoral@glados.(none)>
2013-08-29 14:12:28 +0200

    Included config for BE

---
+++ config/config-be.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+JSPPATH=/var/lib/tomcat6/webapps/cddemo/index.jsp
+
+if [[ "$1" == "" ]]
+then
+	echo "Usage: config-cddemo.sh <MYSQL IP>"
+	exit 1
+fi
+
+if [ ! -f $JSPPATH.orig ]
+then
+	cp $JSPPATH $JSPPATH.orig
+fi
+sed $JSPPATH.orig -e "s/MYSQL_IP/$1/" > $JSPPATH
+
