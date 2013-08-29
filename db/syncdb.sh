aa5519d31fa581155ac08102a589c6c6b81d5a6b
Francisco Mayoral <fmayoral@glados.(none)>
2013-08-29 11:09:10 +0200

    Used env file

--- db/syncdb.sh
+++ db/syncdb.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+source .env
 mysql -u root << EOF
 create database cddemo;
-GRANT ALL ON cddemo.* TO 'cduser'@'localhost' IDENTIFIED BY 'cduser';
-GRANT ALL ON cddemo.* TO 'cduser'@'%' IDENTIFIED BY 'cduser';
+GRANT ALL ON cddemo.* TO '$MYSQL_DB_USER'@'localhost' IDENTIFIED BY '$MYSQL_DB_PASS';
+GRANT ALL ON cddemo.* TO '$MYSQL_DB_USER'@'%' IDENTIFIED BY '$MYSQL_DB_PASS';
 use cddemo;
 create table counter (value INT NOT NULL);
 insert into counter values (1);
